8d1811457e59b47580a15ec4dfa6fbe13c37b122
David Joos <david.joos@gmail.com>
2014-08-04 20:47:51 +0100

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'dev@escapestudios.com'
 license 'MIT'
 description 'Installs/Configures mplayer'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '0.1.1'
 
 %w( debian ubuntu ).each do |os|
   supports os
